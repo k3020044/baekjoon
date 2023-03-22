15178f698944ab99476e0fbcbf0ab9478d6c98b7
Youngjun Kim <109324432+k3020044@users.noreply.github.com>
2023-03-22 09:40:57 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,26 +1,32 @@
-n = int(input())
-# idx 값 맞추기 위해 0번에 미지값 추가
-bulbs = [-1] + list(map(int, input().split()))
-k = int(input())
-students = [tuple(map(int, input().split())) for _ in range(k)]
-
-for sex, num in students:
-    # 성별이 남자일때
-    if sex == 1:
-        for i in range(num, n+1, num):
-            bulbs[i] = 1 - bulbs[i]
-    # 성별이 여자일때
+def change(num):
+    if switch[num] == 0:
+        switch[num] = 1
     else:
-        bulbs[num] = 1 - bulbs[num]
-        for k in range(n//2):
-            if num + k > n or num - k < 1: break
-            if bulbs[num+k] == bulbs[num-k]:
-                bulbs[num+k] = 1 - bulbs[num+k]
-                bulbs[num-k] = 1 - bulbs[num-k]
-            else:
-                break  
+        switch[num] = 0
+    return
 
-for i in range(1, n+1):
-    print(bulbs[i], end=' ')
-    if i % 20 == 0:
+
+N = int(input())
+switch = [-1] + list(map(int, input().split()))
+students = int(input())
+for _ in range(students):
+    sex, num = map(int, input().split())
+    # 남자
+    if sex == 1:
+        for i in range(num, N+1, num):
+            change(i)
+    # 여자
+    else:
+        change(num)
+        for k in range(N//2):
+            if num + k > N or num - k < 1 : break
+            if switch[num + k] == switch[num - k]:
+                change(num + k)
+                change(num - k)
+            else:
+                break
+                
+for i in range(1, N+1):
+    print(switch[i], end = " ")
+    if i % 20 == 0 :
         print()
